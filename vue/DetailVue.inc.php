97d4dc3f2b51ef7fc0a0f04501aa50b47fc6bbf4
Vincent Perrault <vinc-bob@hotmail.ca>
2016-10-27 16:45:48 +0000

    DetailVue.inc.php edited online with Bitbucket

--- vue/DetailVue.inc.php
+++ vue/DetailVue.inc.php
@@ -1,39 +1,38 @@
 <?php
 	require 'libelles_fr.php';
+	$titrePage = $album->getTitre();
 ?>
 
-<h1><?=$album->getTitre();?></h1>
 
-<div>
+<div id="detailAlbum">
 	<img src="images/<?=$album->getImagePochette();?>" alt="images/<?=$album->getImagePochette();?>" />
-	<div>
+	<div id="descriptionAlbum">
 		<p><?=$detailArtiste?>: <?=$album->getNomArtiste();?></p>
 		<p><?=$detailNbPieces?>: <?=$album->getNbPieces();?></p>
 		<p><?=$detailTempsTotal?>: <?=$album->getTempsTotal();?></p>
-		<p><?=$detailLien?>: <a href="<?=$album->getLienArtiste();?>"><?=$album->getLienArtiste();?></a></p>
+		<p><?=$detailLien?>: <a href="<?=$album->getLienArtiste();?>" target='_blank'><?=$album->getLienArtiste();?></a></p>
 	</div>
 </div>
-<div>
-	<form action="index.php">
-		<table>
-			<tr>
-				<th><img src="images/1354785691_sort_ascending.png" alt="" /><?=$detailNumeroPiece?><img src="images/1354785628_sort_descending.png" alt="" /></th>
-				<th><img src="images/1354785691_sort_ascending.png" alt="" /><?=$detailDureePiece?><img src="images/1354785628_sort_descending.png" alt="" /></th>
-				<th><img src="images/1354785691_sort_ascending.png" alt="" /><?=$detailTitrePiece?><img src="images/1354785628_sort_descending.png" alt="" /></th>
-			</tr>
-			<?php 
-				$liste = $album->getListePieces();
+<table id='tableDetailAlbumPieces'>
+	<thead>
+		<tr>
+			<th><img src="images/1354785691_sort_ascending.png" alt="" /><?=$detailNumeroPiece?><img src="images/1354785628_sort_descending.png" alt="" /></th>
+			<th><img src="images/1354785691_sort_ascending.png" alt="" /><?=$detailDureePiece?><img src="images/1354785628_sort_descending.png" alt="" /></th>
+			<th><img src="images/1354785691_sort_ascending.png" alt="" /><?=$detailTitrePiece?><img src="images/1354785628_sort_descending.png" alt="" /></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php 
+			$liste = $album->getListePieces();
 				
-				foreach($liste as $piece){
-					echo "<tr>";
-					echo "<td>" . $piece->getNumero() . "</td>";
-					echo "<td>" . $piece->getDuree() . "</td>";
-					echo "<td>" . $piece->getTitre() . "</td>";
-					echo "</tr>";
-				}
-			?>
-		</table>
-		<input type="submit" value="<?=$detailRetour?>" />
-	</form>
-
-</div>
+			foreach($liste as $piece){
+				echo "<tr>";
+				echo "<td>" . $piece->getNumero() . "</td>";
+				echo "<td>" . $piece->getDuree() . "</td>";
+				echo "<td>" . $piece->getTitre() . "</td>";
+				echo "</tr>";
+			}
+		?>
+	</tbody>
+</table>
+<a href="index.php"><input id="Retour" type="button" value="<?=$detailRetour?>" /></a>	
