c6cc2500b42efbba32f24435be4dd321a21ac1f9
Vincent Perrault <vinc-bob@hotmail.ca>
2016-10-27 16:37:43 +0000

    AjoutVue.inc.php edited online with Bitbucket

--- vue/AjoutVue.inc.php
+++ vue/AjoutVue.inc.php
@@ -1,39 +1,75 @@
 <?php
 	require 'libelles_fr.php';
-	define("MAXINPUTPIECES", 5);
+	$titrePage = $titreAjout;
+	
+	$minInputPieces = 5;
+	if(isset($_POST["ajoutPieces"])) {
+		if (isset($_POST['count'])){
+			$count = $_POST['count'] + 2;
+			$minInputPieces += $count;
+		}
+		else $count = 0;
+	}
+	
+	$titreInputClasse = $artisteInputClasse = $urlArtisteClasse = "ajoutInfosBox";
+	$imagePochetteClasse = "";
+	
+	/*if(isset($_POST['titrePieces']))
+		var_dump($_POST['titrePieces']);*/
+	
+	// Validation des champs du formulaire
+	if(isset($_POST["submit"])) {
+		if (isset($_POST['count'])){
+			$count = $_POST['count'];
+			$minInputPieces += $count;
+		}
+		
+		// Validation du titre de l'album
+		if(isset($_POST['titreAlbum'])){
+			$titreAlbum = $_POST['titreAlbum'];
+			if(strlen(trim($titreAlbum)) < 1 || strlen($titreAlbum) > 40){
+				//echo "Le titre de l'album est plus de 40 caractères ou vide <br />";
+				$titreInputClasse = "erreur";
+			}
+		}
+		
+		// Validation du nom de l'artiste
+		if(isset($_POST['artiste'])){
+			$artiste = $_POST['artiste'];
+			if(strlen(trim($artiste)) < 1 || strlen($artiste) > 40){
+				//echo "Le nom de l'artiste de l'album est plus de 40 caractères ou vide <br />";
+				$artisteInputClasse = "erreur";
+			}
+		}
+		
+		// Validation de l'url de l'artiste
+		if(isset($_POST['urlArtiste'])){
+			$urlArtiste = $_POST['urlArtiste'];
+			if(!preg_match("/^.*[.].*$/", $urlArtiste))
+				$urlArtisteClasse = "erreur";
+		}
+		
+		// Validation avec les pieces musicales à suivre...
+		
+		// Validation de l'image de la pochette
+		if(isset($_POST['imagePochette'])){
+			$imagePochette = $_POST['imagePochette'];
+			if($imagePochette == '')
+				$imagePochetteClasse = "erreur";
+		}
+	}
+	
 ?>
 
-<h1><?=$titreAjout?></h1>
-
-<form>
+<form action="index.php?entite=album&action=ajouter" method="post">
 	<div>
 		<h2><?=$ajoutTitreInformationsBase?></h2>
-		<label class="ajoutInfosLabel"><?=$ajoutInformationsBaseTitre?></label><input type="text" class="ajoutInfosBox" /><br />
-		<label class="ajoutInfosLabel"><?=$ajoutInformationsBaseArtiste?></label><input type="text" class="ajoutInfosBox" /><br />
-		<label class="ajoutInfosLabel"><?=$ajoutInformationsBaseURL?></label><input type="text" class="ajoutInfosBox" />
+		<label class="ajoutInfosLabel"><?=$ajoutInformationsBaseTitre?></label><input type="text" class="<?php echo $titreInputClasse;?>" name="titreAlbum" value="<?php if(isset($_POST['titreAlbum'])) echo $_POST['titreAlbum'];?>"/><br />
+		<label class="ajoutInfosLabel"><?=$ajoutInformationsBaseArtiste?></label><input type="text" class="<?php echo $artisteInputClasse;?>"  name="artiste" value="<?php if(isset($_POST['artiste'])) echo $_POST['artiste'];?>"/><br />
+		<label class="ajoutInfosLabel"><?=$ajoutInformationsBaseURL?></label><input type="text" class="<?php echo $urlArtisteClasse;?>" name="urlArtiste" value="<?php if(isset($_POST['urlArtiste'])) echo $_POST['urlArtiste'];?>"/>
 	</div>
 	<div>
-		<h2><?=$ajoutTitreListePieces?></h2>
-			<!-- Cette partie est Ã  enlever, la bonne est avec le table
-			<div>
-				<div id="ajoutListeTitre">
-					<h3><?=$ajoutListePiecesTitre?></h3>
-				
-					<?php 
-						for($i = 0; $i < 5; $i++){
-							echo "<input type='text' class='ajoutListeBox' name='ajoutListeBox$i'/>";
-						}
-					?>
-				</div>
-				<div id="ajoutListeDuree">
-					<h3><?=$ajoutListeDuree?></h3>
-					<?php 
-						for($i = 0; $i < 5; $i++){
-							echo "<input type='text' class='ajoutListeBox' name='ajoutListeBox$i'/>";
-						}
-					?>
-				</div>
-			</div>-->
+		<h2>"<?=$ajoutTitreListePieces?>"</h2>
 		<table id="tablePieces">
 			<thead>
 				<tr>
@@ -42,24 +78,26 @@
 				</tr>
 			</thead>
 			<tbody>
-				<?php for($i=1; $i <= MAXINPUTPIECES; $i++){?>
+				<?php for($i=0; $i < $minInputPieces; $i++){?>
 				<tr>
-					<td class="titrePiece"><input type='text' class='ajoutListeBox' name='ajoutListeBox$i'/></td>
-					<td class="dureePiece"><input type='text' class='ajoutListeBox' name='ajoutListeBox$i'/></td>
+					<td class="titrePiece"><input type='text' class='ajoutListeBox' name="titrePieces[]" value="<?php //if(isset($_POST['titrePieces'])) echo $_POST['titrePieces'][$i]?>"/></td>
+					<td class="dureePiece"><input type='text' class='ajoutListeBox' name='piece[]' /></td>
 				</tr>
 				<?php }?>
 			</tbody>
 		</table>
-		<input type="button" value="<?=$ajoutListeBouton?>"/>
 	</div>
+	<button type="submit" name="ajoutPieces" formaction="<?php echo $_SERVER['PHP_SELF']."?action=ajouterPieces";?>" value="<?=$ajoutListeBouton?>"><?=$ajoutListeBouton?></button>
 	<div class="sectionBoutons">
 		<h2><?=$ajoutTitreImagePochette?></h2>
 			<div>
-				<input type="file" value="<?=$ajoutBoutonChoisir?>" />
+				<input type="file" name="imagePochette" value="<?=$ajoutBoutonChoisir?>" class="<?php echo $imagePochetteClasse;?>"/>
 			</div>
 	</div>
 	<div class="sectionBoutons">
-			<input type="submit" value="<?=$ajoutBoutonConfirmation?>" />
+			<input type="submit" name="submit" value="<?=$ajoutBoutonConfirmation?>" />
 			<a href="index.php"><input type="button" value="<?=$ajoutBoutonAnnulation?>" /></a>
 	</div>
+	<input type="hidden" name="count" value="<?php if (isset($_POST['count'])) echo $count;?>">
 </form>
+
