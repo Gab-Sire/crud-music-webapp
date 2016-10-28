3c8ef9d977cce11a19675a2fa34b3b034a5cbff9
Vincent Perrault <vinc-bob@hotmail.ca>
2016-10-28 22:00:34 +0000

    AlbumModele.php edited online with Bitbucket j'ai remis ce qui était dans la fonction supprimeALbum

--- modele/AlbumModele.php
+++ modele/AlbumModele.php
@@ -84,7 +84,8 @@ class AlbumModele extends Modele {
 	}
 	
 	public function supprimeAlbum($album){
-		unset($album);
+		//detruit le fichier d'image de pochette de l'album
+		unlink("images/" . $album->getImagePochette());
 	}
 	
 	public function validationTitreAlbum($titre){
