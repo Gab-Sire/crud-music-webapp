7117d2357d57ea953b375900c9577d45f043a33d
Vincent Perrault <vinc-bob@hotmail.ca>
2016-10-31 00:21:39 +0000

    Piece.php edited online with Bitbucket avec entête

--- classe/Piece.php
+++ classe/Piece.php
@@ -1,3 +1,13 @@
+<!-- ******************************************************************
+	Nom du fichier: 	Piece.php
+
+	Auteur:				Vincent Perrault et Gabriel Cyr
+	Date de creation: 	30 octobre 2016
+	Cours-Groupe: 		420-323-AL groupe: 1 et 2
+	
+	But du document:
+		Page qui représente la classe Piece de l'Application
+********************************************************************  -->
 <?php
 
 class Piece {
