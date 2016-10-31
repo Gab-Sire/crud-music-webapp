1735b0b84243bad029136383ae53d5946124d57d
Vincent Perrault <vinc-bob@hotmail.ca>
2016-10-31 00:23:27 +0000

    Modele.php edited online with Bitbucket avec entête

--- modele/Modele.php
+++ modele/Modele.php
@@ -1,5 +1,15 @@
+<!-- ******************************************************************
+	Nom du fichier: 	Modele.php
+
+	Auteur:				Vincent Perrault et Gabriel Cyr
+	Date de creation: 	30 octobre 2016
+	Cours-Groupe: 		420-323-AL groupe: 1 et 2
+	
+	But du document:
+		Page qui représente le modele principal de l'application
+********************************************************************  -->
 <?php
 abstract class Modele {
 	
-	//abstract public function getAll();
+	abstract public function getAll();
 }
