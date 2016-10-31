b45aef411bb54edbe1b35ae509b3270c86f63f5e
Vincent Perrault <vinc-bob@hotmail.ca>
2016-10-31 00:28:44 +0000

    ResultatRechercheVue.inc.php edited online with Bitbucket avec entête

--- vue/ResultatRechercheVue.inc.php
+++ vue/ResultatRechercheVue.inc.php
@@ -1,3 +1,13 @@
+<!-- ******************************************************************
+	Nom du fichier: 	ResultatRechercheVue.inc.php
+
+	Auteur:				Vincent Perrault et Gabriel Cyr
+	Date de creation: 	30 octobre 2016
+	Cours-Groupe: 		420-323-AL groupe: 1 et 2
+	
+	But du document:
+		Page qui représente la vue d'inclusion de recherche dans les albums de l'application
+********************************************************************  -->
 <?php
 	require 'libelles_fr.php';
 	$titrePage = $titreRecherche;
