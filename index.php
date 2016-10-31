29090cccf66aaade1362f27ff08cc65fb92c29fd
Vincent Perrault <vinc-bob@hotmail.ca>
2016-10-31 00:29:07 +0000

    index.php edited online with Bitbucket avec entête

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
+<!-- ******************************************************************
+	Nom du fichier: 	ResultatRechercheVue.inc.php
+
+	Auteur:				Vincent Perrault et Gabriel Cyr
+	Date de creation: 	30 octobre 2016
+	Cours-Groupe: 		420-323-AL groupe: 1 et 2
+	
+	But du document:
+		Page qui représente la vue d'inclusion de recherche dans les albums de l'application
+********************************************************************  -->
 <?php
 
-//crÃ©e et le contrÃ´leur frontal qui aiguille les diffÃ©rentes actions
+//crée et le contrôleur frontal qui aiguille les différentes actions
 require_once("controleur/Controleur.php");
 $controleur = new Controleur();
 $controleur->dispatch();
